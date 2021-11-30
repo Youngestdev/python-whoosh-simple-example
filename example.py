c0bdc93f6e4edfad806824fa43a358c06b78f707
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2021-11-30 01:17:59 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,51 +1,34 @@
+import json
 from typing import Dict, List, Sequence
 
-from whoosh.index import create_in
-from whoosh.fields import *
-from whoosh.qparser import MultifieldParser
-from whoosh.filedb.filestore import RamStorage
 from whoosh.analysis import StemmingAnalyzer
-
-import json
-
-#
-# Simple example indexing to an in-memory index and performing a search
-# across multiple fields and returning an array of highlighted results.
-#
-# One lacking feature of Whoosh is the no-analyze option. In this example
-# the SearchEngine modifies the given schema and adds a RAW field. When doc
-# are added to the index only stored fields in the schema are passed to Whoosh
-# along with json encoded version of the whole doc stashed in the RAW field.
-#
-# On query the <Hit> in the result is ignored and instead the RAW field is
-# decoded containing any extra fields present in the original document. 
-#
+from whoosh.fields import *
+from whoosh.filedb.filestore import RamStorage
+from whoosh.qparser import MultifieldParser
 
 
-class SearchEngine:
+class Indexer:
 
     def __init__(self, schema):
         self.schema = schema
-        schema.add('raw', TEXT(stored=True))
+        schema.add('crawled_sls_data', TEXT(stored=True))
         self.ix = RamStorage().create_index(self.schema)
 
     def index_documents(self, docs: Sequence):
         writer = self.ix.writer()
+
         for doc in docs:
-            d = {k: v for k,v in doc.items() if k in self.schema.stored_names()}
-            d['raw'] = json.dumps(doc) # raw version of all of doc
+            d = {k: v for k, v in doc.items() if k in self.schema.stored_names()}
+            d['crawled_sls_data'] = json.dumps(doc)  # raw version of all of doc
             writer.add_document(**d)
         writer.commit(optimize=True)
 
-    def get_index_size(self) -> int:
-        return self.ix.doc_count_all()
-
-    def query(self, q: str, fields: Sequence, highlight: bool=True) -> List[Dict]:
+    def query(self, q: str, fields: Sequence, highlight: bool = True) -> List[Dict]:
         search_results = []
         with self.ix.searcher() as searcher:
             results = searcher.search(MultifieldParser(fields, schema=self.schema).parse(q))
             for r in results:
-                d = json.loads(r['raw'])
+                d = json.loads(r['crawled_sls_data'])
                 if highlight:
                     for f in fields:
                         if r[f] and isinstance(r[f], str):
@@ -55,47 +38,53 @@ class SearchEngine:
 
         return search_results
 
+
 if __name__ == '__main__':
 
-    docs = [
-        {
-            "id": "1",
-            "title": "First document banana",
-            "description": "This is the first document we've added in San Francisco!",
-            "tags": ['foo', 'bar'],
-            "extra": "kittens and cats"
-        },
-        {
-            "id": "2",
-            "title": "Second document hatstand",
-            "description": "The second one is even more interesting!",
-            "tags": ['alice'],
-            "extra": "foals and horses"
-        },
-        {
-            "id": "3",
-            "title": "Third document slug",
-            "description": "The third one is less interesting!",
-            "tags": ['bob'],
-            "extra": "bunny and rabbit"
-        },
-    ]
+    # docs = [
+    #     {
+    #         "publication_title": "First document banana",
+    #         "publication_url": "This is the first document we've added in San Francisco!",
+    #         "tags": ['foo', 'bar'],
+    #         "authors": ["Abdul"],
+    #         "pub_date": "2020-10-20"
+    #     },
+    #     {
+    #         "publication_title": "Second document",
+    #         "publication_url": "This is the second document we've added in San Francisco!",
+    #         "tags": ['foo', 'bob'],
+    #         "authors_name": [
+    #             'Remi'
+    #         ],
+    #         "pub_date": "2020-10-20"
+    #     },
+    #
+    # ]
+
+    json_file = open("experimental.json")
+    docs = json.load(json_file)
+
+    # Expensive play.
+
+    result = []
+
+    # for key in range(len(docs)):
+    #     docs[key]["authors_name"] = docs[key]["authors_name".replace('"', '')]
 
     schema = Schema(
-        id=ID(stored=True),
-        title=TEXT(stored=True),
-        description=TEXT(stored=True, analyzer=StemmingAnalyzer()),
-        tags=KEYWORD(stored=True)
+        publication_title=TEXT(stored=True, analyzer=StemmingAnalyzer()),
+        publication_url=TEXT(stored=True),
+        authors_name=KEYWORD(stored=True, commas=True, scorable=True),
+        profile_links=KEYWORD(stored=True, commas=True, scorable=True),
+        pub_date=TEXT(stored=True)
     )
 
-    engine = SearchEngine(schema)
+    engine = Indexer(schema)
     engine.index_documents(docs)
 
-    print(f"indexed {engine.get_index_size()} documents")
+    fields_to_search = ["publication_title", "publication_url", "authors_name"]
 
-    fields_to_search = ["title", "description", "tags"]
-
-    for q in ["hatstand", "banana", "first", "second", "alice", "bob", "san francisco"]:
+    for q in ["Horton, E.", "Blood cancer", "Acute", "cancer"]:
         print(f"Query:: {q}")
         print("\t", engine.query(q, fields_to_search, highlight=True))
-        print("-"*70)
+        print("-" * 70)
